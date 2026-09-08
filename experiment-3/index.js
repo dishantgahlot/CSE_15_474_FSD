b263fae0b93c44725a8421ead8563bc8bf6ac79d
Dishant Gahlot <dishantghalot9@gmail.com>
2026-09-08 11:18:10 +0530

    modify index .js

--- experiment-3/index.js
+++ experiment-3/index.js
@@ -1,7 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello Server');
+
+    console.log('HTTP Method:', req.method);
+    console.log('URL:', req.url);
+
+    if (req.method === 'GET' && req.url === '/') {
+        res.end('Home Page');
+    }
+
+    else if (req.method === 'GET' && req.url === '/about') {
+        res.end('About Page');
+    }
+
+    else if (req.method === 'POST' && req.url === '/users') {
+        res.end('User Created');
+    }
+
+    else {
+        res.statusCode = 404;
+        res.end('Route Not Found');
+    }
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
