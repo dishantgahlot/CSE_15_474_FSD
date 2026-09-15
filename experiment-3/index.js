42158e733f1d68205a1b5f76ff7ad8a7f29aaa43
Dishant Gahlot <dishantghalot9@gmail.com>
2026-09-15 11:52:43 +0530

    exp3

--- experiment-3/index.js
+++ experiment-3/index.js
@@ -1,28 +1,76 @@
-const http = require('http');
+const express = require("express");
+const { buildSchema, graphql } = require("graphql");
 
-const server = http.createServer((req, res) => {
+const app = express();
+const port = Number(process.env.PORT) || 4000;
 
-    console.log('HTTP Method:', req.method);
-    console.log('URL:', req.url);
+// GraphQL Schema
+const schema = buildSchema(`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
 
-    if (req.method === 'GET' && req.url === '/') {
-        res.end('Home Page');
-    }
+  type Query {
+    hello: String!
+    user(id: ID!): User
+  }
+`);
 
-    else if (req.method === 'GET' && req.url === '/about') {
-        res.end('About Page');
-    }
+// Dummy data
+const users = [
+  {
+    id: "1",
+    name: "Aman",
+    email: "aman@gmail.com",
+  },
+  {
+    id: "2",
+    name: "Rahul",
+    email: "rahul@gmail.com",
+  },
+];
 
-    else if (req.method === 'POST' && req.url === '/users') {
-        res.end('User Created');
-    }
+// Resolvers
+const root = {
+  hello: () => {
+    return "Hello GraphQL!";
+  },
 
-    else {
-        res.statusCode = 404;
-        res.end('Route Not Found');
-    }Y
+  user: ({ id }) => {
+    return users.find((user) => user.id === id);
+  },
+};
+
+app.use(express.json());
+
+// GraphQL endpoint. Send a query in the request body, for example:
+// POST /graphql  { "query": "{ hello }" }
+app.all("/graphql", async (req, res) => {
+  const query = req.method === "GET" ? req.query.query : req.body?.query;
+
+  if (!query) {
+    return res.status(400).json({
+      errors: [{ message: "Provide a GraphQL query in ?query= or in the JSON request body." }],
+    });
+  }
+
+  try {
+    const result = await graphql({
+      schema,
+      source: query,
+      rootValue: root,
+    });
+    return res.json(result);
+  } catch (error) {
+    return res.status(500).json({
+      errors: [{ message: error.message }],
+    });
+  }
 });
 
-server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/graphql`);
 });
